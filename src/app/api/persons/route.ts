3de2b1527c9c1df2fc074590a9b15747e243b846
pereyrago <pereyrago@gmail.com>
2025-08-24 01:20:26 -0300

    feat(api/persons): create route

---
+++ src/app/api/persons/route.ts
@@ -0,0 +1,40 @@
+export async function GET(request: Request) {
+  const params = new URL(request.url).searchParams;
+  const EXTERNAL_API_URL = process.env.EXTERNAL_API_URL;
+  const SEED = process.env.REQUEST_SEED || "abc";
+  const page = params.get("page") || "1";
+  const perPage = "2000";
+  const inc = [
+    "gender",
+    "name",
+    "location",
+    "email",
+    "phone",
+    "cell",
+    "picture",
+  ];
+
+  try {
+    const apiUrl = `${EXTERNAL_API_URL}api/?page=${page}&results=${perPage}&seed=${SEED}&inc=${inc.join(
+      ","
+    )}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to fetch persons" }), {
+      status: 500,
+      statusText: "Internal Server Error",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
