e78bc4a76c6c4e5ec78c85cccdf503e71b8d8a6a
elsita-dev <elsapallona@gmail.com>
2025-08-24 10:20:05 -0300

    style(pages/dashboard): move props

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,11 +7,12 @@ import { LogOut } from "lucide-react";
 import * as React from "react";
 import { CustomTable } from "@/components/table";
 import Image from "next/image";
+import ThemeToggle from "@/components/theme-switcher";
 
 export default function DashboardPage() {
   const session = useSession();
   const { logout } = session;
-  const { page, fetchPersons, persons, nextPage, prevPage } = usePerson();
+  const { fetchPersons, persons } = usePerson();
 
   useEffect(() => {
     if (!persons || persons.length === 0) fetchPersons(1);
@@ -29,17 +30,12 @@ export default function DashboardPage() {
             className="mx-auto mb-4"
           />
         </div>
+        <ThemeToggle />
         <Button size={"sm"} variant={"outline"} onClick={logout}>
           <LogOut className="mr-2 h-4 w-4" /> Cerrar sesión
         </Button>
       </div>
-      <CustomTable
-        data={persons || []}
-        page={page}
-        onNextPage={nextPage}
-        onPrevPage={prevPage}
-        pageSize={20}
-      />
+      <CustomTable data={persons || []} pageSize={20} />
     </div>
   );
 }
