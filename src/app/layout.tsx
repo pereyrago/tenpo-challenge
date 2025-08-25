25963465a45bdb951efbadd1bd69cee356b91497
elsita-dev <elsapallona@gmail.com>
2025-08-25 08:34:58 -0300

    fix(app): add og metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeWrapper";
 
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  title: "Tekton / Tenpo Challenge",
+  description:
+    "Challenge técnico hecho por Gabriel Pereyra para Tekton Labs | Tenpo.",
+  openGraph: {
+    images: ["/images/og-img.webp"],
+  },
+};
 
 export default function RootLayout({
   children,
