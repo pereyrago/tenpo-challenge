32133cd915c19f196187d55603f97a90db792ff1
elsita-dev <elsapallona@gmail.com>
2025-08-24 23:01:22 -0300

    feat(component): add aclaration component

---
+++ src/components/aclaration/index.tsx
@@ -0,0 +1,40 @@
+export default function Aclaration() {
+  return (
+    <p className="text-gray-600 text-sm bg-white p-2 rounded">
+      Challenge técnico para{" "}
+      <a
+        href="https://www.tektonlabs.com/"
+        rel="noreferrer noopenner"
+        target="_blank"
+        className="font-bold"
+      >
+        Tekton labs
+      </a>{" "}
+      /{" "}
+      <a
+        href="https://www.tenpo.cl/"
+        rel="noreferrer noopenner"
+        target="_blank"
+        className="font-bold"
+      >
+        Tenpo
+      </a>
+      , hecho por{" "}
+      <a
+        href="https://www.linkedin.com/in/gabi-pereyra/"
+        target="_blank"
+        rel="noreferrer noopenner"
+      >
+        Gabriel Pereyra
+      </a>{" "}
+      <a
+        href="https://pereyrago.dev/"
+        target="_blank"
+        rel="noreferrer noopenner"
+        className="font-bold"
+      >
+        pereyrago.dev
+      </a>
+    </p>
+  );
+}
